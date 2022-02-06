8a968bd875f1bbc508407fc9d824a51fe178fd5d
cgcostume <cgdaniel@outlook.com>
2022-02-06 18:15:44 +0100

    try fix deploy action

---
+++ webpack.dev.mjs
@@ -0,0 +1,24 @@
+
+import path from 'path';
+
+import { merge } from 'webpack-merge';
+import common from './webpack.common.mjs';
+
+
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default function (env) {
+    const config = common(env, __dirname);
+    return merge(config,
+        {
+            mode: 'development',
+            cache: true,
+            devServer: {
+                hot: false
+            },
+            optimization: {
+                minimize: true,
+            },
+        });
+}
